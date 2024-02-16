99c353b6295037fd4913baa5be4413e62b0e33f7
Srikanth <Srikanth@webappclouds.com>
2024-02-16 12:21:49 +0530

    Initial Commit

---
+++ Pasquinade/PasquinadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasquinadeApp.swift
+//  Pasquinade
+//
+//  Created by Srikanth on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct PasquinadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
